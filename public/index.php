53566e367a88fd2b4a30323deb4c2c5697162cd9
nicolas-droppa <droppa.nicolas@gmail.com>
2025-05-30 00:12:16 +0200

    fixed favicon path

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ require __DIR__ . '/../vendor/autoload.php';
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>MyAimBuddy</title>
-    <link rel="icon" href="/images/favicon/favicon.ico" sizes="16x16" type="image/png" />
+    <link rel="icon" href="./images/favicon/favicon.ico" sizes="16x16" type="image/png" />
     <link rel="stylesheet" href="styles/style.css"/>
     <link rel="stylesheet" href="styles/fontawesome/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/opencv.js"></script>
